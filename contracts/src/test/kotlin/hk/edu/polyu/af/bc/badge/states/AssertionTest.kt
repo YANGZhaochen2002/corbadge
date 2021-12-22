de7f04237d9c5f72867b01a6625d1c152d0e8186
McXD <fyl155165@gmail.com>
2021-12-22 20:45:02 +0800

    Downgrade kotlin and gradle

--- contracts/src/test/kotlin/hk/edu/polyu/af/bc/badge/states/AssertionTest.kt
+++ contracts/src/test/kotlin/hk/edu/polyu/af/bc/badge/states/AssertionTest.kt
@@ -3,9 +3,8 @@ package hk.edu.polyu.af.bc.badge.states
 import net.corda.core.contracts.UniqueIdentifier
 import net.corda.core.identity.Party
 import net.corda.testing.core.TestIdentity
-import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers.equalTo
 import org.junit.Test
+import kotlin.test.assertEquals
 
 internal class AssertionTest {
     private val me: Party = TestIdentity.fresh("AFBlockchain").party
@@ -15,8 +14,8 @@ internal class AssertionTest {
     fun `can create an assertion`() {
         val assertion = Assertion(badgeClass, me, UniqueIdentifier())
 
-        assertThat(assertion.holder, equalTo(me))
-        assertThat(assertion.issuer, equalTo(me))
-        assertThat(assertion.issuedTokenType.tokenType.tokenClass, equalTo(BadgeClass::class.java))
+        assertEquals(assertion.holder, me)
+        assertEquals(assertion.issuer, me)
+        assertEquals(assertion.issuedTokenType.tokenType.tokenClass, BadgeClass::class.java)
     }
 }
