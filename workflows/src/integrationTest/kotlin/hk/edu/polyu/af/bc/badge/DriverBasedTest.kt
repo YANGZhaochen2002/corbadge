eafa76e7fe05741d787572e4e440bae3f1e10b93
McXD <fyl155165@gmail.com>
2021-12-26 15:01:25 +0800

    Add driver-based test template

--- workflows/src/integrationTest/kotlin/hk/edu/polyu/af/bc/badge/DriverBasedTest.kt
+++ workflows/src/integrationTest/kotlin/hk/edu/polyu/af/bc/badge/DriverBasedTest.kt
@@ -1,47 +1,31 @@
 package hk.edu.polyu.af.bc.badge
 
-import net.corda.core.identity.CordaX500Name
-import net.corda.core.utilities.getOrThrow
-import net.corda.testing.core.TestIdentity
-import net.corda.testing.driver.DriverDSL
-import net.corda.testing.driver.DriverParameters
+import com.github.manosbatsis.corda.testacles.nodedriver.NodeHandles
+import com.github.manosbatsis.corda.testacles.nodedriver.config.NodeDriverNodesConfig
+import com.github.manosbatsis.corda.testacles.nodedriver.jupiter.NodeDriverExtensionConfig
+import com.github.manosbatsis.corda.testacles.nodedriver.jupiter.NodeDriverNetworkExtension
 import net.corda.testing.driver.NodeHandle
-import net.corda.testing.driver.driver
-import org.junit.Test
-import java.util.concurrent.Future
-import kotlin.test.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import kotlin.test.assertTrue
 
+@ExtendWith(NodeDriverNetworkExtension::class)
 class DriverBasedTest {
-    private val bankA = TestIdentity(CordaX500Name("BankA", "", "GB"))
-    private val bankB = TestIdentity(CordaX500Name("BankB", "", "US"))
-
-    @Test
-    fun `node test`() = withDriver {
-        // Start a pair of nodes and wait for them both to be ready.
-        val (partyAHandle, partyBHandle) = startNodes(bankA, bankB)
-
-        // From each node, make an RPC call to retrieve another node's name from the network map, to verify that the
-        // nodes have started and can communicate.
-
-        // This is a very basic test: in practice tests would be starting flows, and verifying the states in the vault
-        // and other important metrics to ensure that your CorDapp is working as intended.
-        assertEquals(bankB.name, partyAHandle.resolveName(bankB.name))
-        assertEquals(bankA.name, partyBHandle.resolveName(bankA.name))
+    companion object {
+        val logger: Logger = LoggerFactory.getLogger(DriverBasedTest::class.java)
+        
+        @NodeDriverExtensionConfig
+        @JvmStatic
+        val nodeDriverConfig: NodeDriverNodesConfig = customNodeDriverConfig
     }
-
-    // Runs a test inside the Driver DSL, which provides useful functions for starting nodes, etc.
-    private fun withDriver(test: DriverDSL.() -> Unit) = driver(
-        DriverParameters(isDebug = true, startNodesInProcess = true)
-    ) { test() }
-
-    // Makes an RPC call to retrieve another node's name from the network map.
-    private fun NodeHandle.resolveName(name: CordaX500Name) = rpc.wellKnownPartyFromX500Name(name)!!.name
-
-    // Resolves a list of futures to a list of the promised values.
-    private fun <T> List<Future<T>>.waitForAll(): List<T> = map { it.getOrThrow() }
-
-    // Starts multiple nodes simultaneously, then waits for them all to be ready.
-    private fun DriverDSL.startNodes(vararg identities: TestIdentity) = identities
-        .map { startNode(providedName = it.name) }
-        .waitForAll()
+    
+    @Test
+    fun `can retrieve node identity`(nodeHandles: NodeHandles) {
+        val nodeA: NodeHandle = nodeHandles.getNode("partyA")
+        assertTrue(nodeA.nodeInfo.legalIdentities.isNotEmpty())
+        
+        logger.info("Retrieved nodeA identity: ${nodeA.nodeInfo.legalIdentities[0]}")
+    }
 }
---
+++ workflows/src/integrationTest/kotlin/hk/edu/polyu/af/bc/badge/Utils.kt
@@ -0,0 +1,20 @@
+package hk.edu.polyu.af.bc.badge
+
+import com.github.manosbatsis.corda.testacles.nodedriver.NodeParamsHelper
+import com.github.manosbatsis.corda.testacles.nodedriver.config.NodeDriverNodesConfig
+import com.github.manosbatsis.corda.testacles.nodedriver.config.SimpleNodeDriverNodesConfig
+import com.r3.corda.lib.tokens.workflows.flows.rpc.CreateEvolvableTokens
+import hk.edu.polyu.af.bc.badge.flows.CreateBadgeClass
+import net.corda.testing.core.ALICE_NAME
+import net.corda.testing.core.BOB_NAME
+
+val nodeParamsHelper = NodeParamsHelper()
+
+val customNodeDriverConfig: NodeDriverNodesConfig =
+        SimpleNodeDriverNodesConfig (
+                cordappProjectPackage = CreateBadgeClass::class.java.`package`.name,
+                cordappPackages = listOf<String>(CreateEvolvableTokens::class.java.`package`.name),
+                nodes = mapOf("partyA" to nodeParamsHelper.toNodeParams(ALICE_NAME), "partyB" to nodeParamsHelper.toNodeParams(BOB_NAME)),
+                minimumPlatformVersion = 4,
+                debug = false
+        )
