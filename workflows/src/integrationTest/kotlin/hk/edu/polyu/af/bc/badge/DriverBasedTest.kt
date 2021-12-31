56c7b561866896dd49cac159785dd5a5e28b8adb
McXD <fyl155165@gmail.com>
2021-12-31 18:15:06 +0800

    Write driver-based integration test

--- workflows/src/integrationTest/kotlin/hk/edu/polyu/af/bc/badge/DriverBasedTest.kt
+++ workflows/src/integrationTest/kotlin/hk/edu/polyu/af/bc/badge/DriverBasedTest.kt
@@ -4,17 +4,73 @@ import com.github.manosbatsis.corda.testacles.nodedriver.NodeHandles
 import com.github.manosbatsis.corda.testacles.nodedriver.config.NodeDriverNodesConfig
 import com.github.manosbatsis.corda.testacles.nodedriver.jupiter.NodeDriverExtensionConfig
 import com.github.manosbatsis.corda.testacles.nodedriver.jupiter.NodeDriverNetworkExtension
+import hk.edu.polyu.af.bc.badge.flows.CreateBadgeClass
+import hk.edu.polyu.af.bc.badge.flows.IssueAssertion
+import hk.edu.polyu.af.bc.badge.states.Assertion
+import hk.edu.polyu.af.bc.badge.states.BadgeClass
+import net.corda.core.messaging.CordaRPCOps
 import net.corda.testing.driver.NodeHandle
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import kotlin.test.assertTrue
 
-//@ExtendWith(NodeDriverNetworkExtension::class)
+@Disabled("Error: multiple jar files found for the same flow class")
+@ExtendWith(NodeDriverNetworkExtension::class)
 class DriverBasedTest {
+    companion object {
+        val logger: Logger = LoggerFactory.getLogger(DriverBasedTest::class.java)
+
+        @NodeDriverExtensionConfig
+        @JvmStatic
+        val nodeDriverConfig: NodeDriverNodesConfig = customNodeDriverConfig
+
+        val badgeClasses: MutableMap<String, BadgeClass> = mutableMapOf()
+    }
+
     @Test
-    fun test() {
-        println("test")
+    @Order(1)
+    fun canRetrieveIdentity(nodeHandles: NodeHandles) {
+        val nodeA: NodeHandle = nodeHandles.getNode("partyA")
+        assertTrue(nodeA.nodeInfo.legalIdentities.isNotEmpty())
+
+        logger.info("Retrieved nodeA identity: ${nodeA.nodeInfo.legalIdentities[0]}")
+    }
+
+    @Test
+    @Order(2)
+    fun canCreateBadgeClassAtPartyA(nodeHandles: NodeHandles) {
+        val nodeA: NodeHandle = nodeHandles.getNode("partyA")
+        val proxy: CordaRPCOps = nodeA.rpc
+
+        val tx = proxy.startFlowDynamic(CreateBadgeClass::class.java, "test", "test").returnValue.get()
+        logger.info("Transaction: {}", tx.toString())
+        val badgeClass = tx.coreTransaction.outputsOfType(BadgeClass::class.java)[0]
+        badgeClasses["a"] = badgeClass
+        logger.info("BadgeClass: {}", badgeClass.toString())
+
+        assert(proxy.vaultQuery(BadgeClass::class.java).states.stream().anyMatch { (state) -> state.data.linearId == badgeClass.linearId })
+    }
+
+    @Test
+    @Order(3)
+    fun canIssueAssertionFromPartyAToPartyB(nodeHandles: NodeHandles) {
+        val nodeA: NodeHandle = nodeHandles.getNode("partyA")
+        val nodeB: NodeHandle = nodeHandles.getNode("partyB")
+        val issProxy: CordaRPCOps = nodeA.rpc
+        val recProxy: CordaRPCOps = nodeB.rpc
+
+        val createdBadgeClass = badgeClasses["a"]!!
+        val tx = issProxy.startFlowDynamic(IssueAssertion::class.java, createdBadgeClass.toPointer(BadgeClass::class.java),
+                recProxy.nodeInfo().legalIdentities[0]).returnValue.get()
+        logger.info("Transaction: {}", tx.toString())
+        val assertion = tx.coreTransaction.outputsOfType(Assertion::class.java)[0]
+        logger.info("Assertion: {}", assertion.toString())
+
+        assert(issProxy.vaultQuery(Assertion::class.java).states.stream().anyMatch { (state) -> state.data.linearId == assertion.linearId })
+        assert(recProxy.vaultQuery(Assertion::class.java).states.stream().anyMatch { (state) -> state.data.linearId == assertion.linearId })
     }
 }
--- workflows/src/integrationTest/kotlin/hk/edu/polyu/af/bc/badge/Utils.kt
+++ workflows/src/integrationTest/kotlin/hk/edu/polyu/af/bc/badge/Utils.kt
@@ -3,16 +3,9 @@ package hk.edu.polyu.af.bc.badge
 import com.github.manosbatsis.corda.testacles.nodedriver.NodeParamsHelper
 import com.github.manosbatsis.corda.testacles.nodedriver.config.NodeDriverNodesConfig
 import com.github.manosbatsis.corda.testacles.nodedriver.config.SimpleNodeDriverNodesConfig
-import com.r3.corda.lib.tokens.workflows.flows.rpc.CreateEvolvableTokens
 import hk.edu.polyu.af.bc.badge.flows.CreateBadgeClass
-import net.corda.client.rpc.CordaRPCConnection
 import net.corda.testing.core.ALICE_NAME
 import net.corda.testing.core.BOB_NAME
-import net.corda.client.rpc.CordaRPCClient
-
-import net.corda.core.utilities.NetworkHostAndPort
-
-
 
 
 val nodeParamsHelper = NodeParamsHelper()
@@ -20,7 +13,12 @@ val nodeParamsHelper = NodeParamsHelper()
 val customNodeDriverConfig: NodeDriverNodesConfig =
         SimpleNodeDriverNodesConfig (
                 cordappProjectPackage = CreateBadgeClass::class.java.`package`.name,
-                cordappPackages = listOf<String>(CreateEvolvableTokens::class.java.`package`.name),
+                cordappPackages = listOf(
+                        "hk.edu.polyu.af.bc.badge.contracts",
+                        "hk.edu.polyu.af.bc.badge.flows",
+                        "com.r3.corda.lib.tokens.workflows.flows",
+                        "com.r3.corda.lib.tokens.contracts"
+                ),
                 nodes = mapOf("partyA" to nodeParamsHelper.toNodeParams(ALICE_NAME), "partyB" to nodeParamsHelper.toNodeParams(BOB_NAME)),
                 minimumPlatformVersion = 4,
                 debug = false
