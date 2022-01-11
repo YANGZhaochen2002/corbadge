4d3b171aaa66f0e72c4756d0a2644e358bb53f17
YANGZhaochen2002 <93120280+YANGZhaochen2002@users.noreply.github.com>
2022-01-11 14:50:17 +0800

    Update Assertion.kt

--- contracts/src/main/kotlin/hk/edu/polyu/af/bc/badge/states/Assertion.kt
+++ contracts/src/main/kotlin/hk/edu/polyu/af/bc/badge/states/Assertion.kt
@@ -8,18 +8,20 @@ import net.corda.core.contracts.BelongsToContract
 import net.corda.core.contracts.UniqueIdentifier
 import net.corda.core.identity.AbstractParty
 import net.corda.core.identity.Party
+import java.sql.Time
+import java.util.*
 
 @BelongsToContract(NonFungibleTokenContract::class) // use generic NFT-Contract
 class Assertion(
         private val badgeClassPointer: TokenPointer<BadgeClass>,
         override val issuer: Party, // TODO: issuer must be the same as the issuer in the badgeClass. Check this in contract
         private val recipient: AbstractParty,
+        private val issuedOn: Date,
+        private val revoked: Boolean,
         override val linearId: UniqueIdentifier
+
 ): NonFungibleToken(
         IssuedTokenType(issuer, badgeClassPointer),
         recipient,
         linearId
-){
-    override val participants: List<AbstractParty>
-        get() = listOf(issuer, recipient) // default is only recipient
-}
+)
